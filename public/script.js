ac1363fb95e5f621f8235f4a5cfd4e3c555f0e3b
Daniel Martins <62864768+danismartins19@users.noreply.github.com>
2021-12-14 12:15:12 -0300

    Exibição de imagens da API

---
+++ public/script.js
@@ -0,0 +1,49 @@
+//variables
+
+let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+let imageContainer = document.querySelector('.image');
+
+
+
+
+//click events
+
+document.querySelector('button').addEventListener('click' , () =>{
+    getExternalImage();
+})
+
+document.querySelector('.image').addEventListener('click', () => {
+    
+    let imageSource = document.querySelector('.image img').src;
+    const index = favorites.indexOf(imageSource);
+
+    if(index == -1){
+        favorites.push(imageSource);
+        imageContainer.classList.add('fav');
+        localStorage.setItem('favorites', JSON.stringify(favorites))
+    } else {
+        favorites.splice(index, 1);
+        imageContainer.classList.remove('fav');
+        localStorage.setItem('favorites', JSON.stringify(favorites))
+    }
+
+})
+
+//methods
+
+const getExternalImage = async ()=> {
+    
+
+    const response = await fetch('https://source.unsplash.com/random')
+
+    document.querySelector('.image').innerHTML = `<img src= "${response.url}">`
+
+    let checkIndex = favorites.indexOf(response.url);
+    if(checkIndex == -1){
+        imageContainer.classList.remove('fav');
+    }else{
+        imageContainer.classList.add('fav');
+    }
+}
+
+getExternalImage();
