31b837a0e840c40beb1c7a1fe0ebebb9a0efad71
Daniel Martins <danidiamoo1@gmail.com>
2021-12-22 09:07:20 -0300

    navegacao do modal implementado completamente

--- public/album.js
+++ public/album.js
@@ -1,6 +1,8 @@
 const c = el => document.querySelector(el);
 let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 let buttonBack = c('.return');
+let indexPhoto = 0;
+let modalOpen = false;
 
 favorites.map((photo, index)=>{
     let modelPhoto = c('.imgLiked').cloneNode(true);
@@ -9,14 +11,60 @@ favorites.map((photo, index)=>{
 
     modelPhoto.addEventListener('click' , (e) =>{
         e.preventDefault();
-
-        let indexPhoto = e.target.closest('.imgLiked').getAttribute('data-key');
-        
-
+        indexPhoto = e.target.closest('.imgLiked').getAttribute('data-key');
+        updateImage(indexPhoto);
     })
 
     c( '.album' ).append( modelPhoto );
 })
+
+
+
+//functions
+
+const updateImage = (indexPhotoModal) =>{
+
+    modalOpen = true;
+
+    document.querySelector('.modal--Image').src = favorites[indexPhotoModal];    
+    document.querySelector('.modal').style.display = 'flex';
+}
+
+const closeModal = () =>{
+    document.querySelector('.modal').style.display = 'none';
+}
+
+
+//events
+
+document.addEventListener('keydown', (e) =>{
+    if((e.key === "Escape" || e.key === "Esc") && (modalOpen)){
+        modalOpen = false;
+        closeModal();
+    }
+})
+
+document.querySelector('.closeModal').addEventListener('click' , () =>{
+    closeModal();
+})
+
+document.querySelector('.next--Image').addEventListener('click' , () =>{
+    if(indexPhoto < (favorites.length - 1)){
+        indexPhoto++;
+        updateImage(indexPhoto);
+        console.log('index: '+indexPhoto + ' tamanho do array: ' + favorites.length);
+    }
+})
+
+document.querySelector('.previous--Image').addEventListener('click' , () =>{
+    if(indexPhoto >= 1){
+        indexPhoto--;
+        updateImage(indexPhoto);
+
+    }
+})
+
+
 
 buttonBack.addEventListener('click' , () =>{
     window.open('../index.html', '_self');
